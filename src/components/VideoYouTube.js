0598ecd3c0bf3d65cc96a18aa127f9e4f809fa5a
Federico Aloi <federico.aloi@gmail.com>
2021-08-10 14:48:42 -0300

    Extraido componente VideoYouTube

---
+++ src/components/VideoYouTube.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export const VideoYouTube = ({ id, titulo, descripcion }) => (
+  <div className="card">
+    <div className="card__image">
+      <iframe
+        src={`https://www.youtube.com/embed/${id}`}
+        className="post-content-video"
+        frameBorder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+    </div>
+    <div className="card__body">
+      <h3>{titulo}</h3>
+      <p>{descripcion}</p>
+    </div>
+  </div>
+);
